149cc538e61f1a54ef4ce4fa341eeaeecb6b2ce2
Akoruu.Dev <akoruu.dev@gmail.com>
2022-12-09 01:04:46 -0300

    FEAT [Games Controller]: Create controllers from games page on API

---
+++ src/controllers/games.controller.js
@@ -0,0 +1,40 @@
+import { connection } from "../../database/database.js";
+
+export async function getGames ( req, res ) {
+    const list = await connection.query(`SELECT * FROM games`);
+
+    res.send(list);
+};
+
+export async function postGame ( req, res ) {
+    const { name, image, stockTotal, categoryId, pricePerDay } = req.body;
+
+    /* name: Joi
+            .string()
+            .request()
+            .min(1),
+    stockTotal: Joi
+            .number()
+            .request()
+            .value(> 0),
+    pricePerDay: Joi
+            .number()
+            .request()
+            .value(> 0) 
+    
+    return 400        */
+
+    const check = await connection.query(`SELECT * FROM games WHERE id = $1` [categoryId]);
+
+    if(!check) {
+        res.status(400).send(`This categoryId doesn't exists`);
+    }
+
+    const verifyName = await connection.query(`SELECT ( name ) FROM games WHERE name = $1`, [name]);
+
+    if (verifyName) {
+        res.status(409).send(`This game name already exists`);
+    };
+
+    res.status(201).send(`This game added successfully`);
+};
--- src/routes/games.routes.js
+++ src/routes/games.routes.js
@@ -1,8 +1,9 @@
 import { Router } from "express";
+import { getGames, postGame } from "../controllers/games.controller.js";
 
 const games = Router();
 
-games.get('/games', () => {});
-games.post('/games', () => {});
+games.get('/games', getGames);
+games.post('/games', postGame);
 
 export default games;
